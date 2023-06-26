d8f498408000a2fc3d03f927d09747171988be8e
andv <andv@zhaw.ch>
2023-06-26 14:49:35 +0200

    Adds 'automated_tests' section

---
+++ automated_tests/wing/run_wing.py
@@ -0,0 +1,88 @@
+from adflow_util import ADFLOW_UTIL
+import argparse
+import os
+import numpy as np
+
+
+def solveAdjoint(solver, ap, n):
+    funcsSens = {}
+    # DVCon.evalFunctionsSens(funcsSens)
+    solver.evalFunctionsSens(ap, funcsSens)
+    solver.checkAdjointFailure(ap, funcsSens)
+
+
+defaultFuncList = [
+    "lift",
+    "drag",
+    "cl",
+    "cd",
+    "fx",
+    "fy",
+    "fz",
+    "cfx",
+    "cfy",
+    "cfz",
+    "mx",
+    "my",
+    "mz",
+    "cmx",
+    "cmy",
+    "cmz",
+    "sepsensor",
+    "sepsensoravgx",
+    "sepsensoravgy",
+    "sepsensoravgz",
+]
+
+options = {
+    'name': "mdo_tutorial",
+    'autoRestart': False,
+    "postRunCallback": solveAdjoint
+}
+
+aeroOptions = {
+        'alpha': 1.8,
+        'mach': 0.80,
+        'P': 20000.0,
+        'T': 220.0,
+        'areaRef':45.5,
+        'chordRef':3.25,
+        'beta':0.0,
+        'R':287.87,
+        'xRef':0.0,
+        'yRef':0.0,
+        'zRef':0.0,
+        'evalFuncs':defaultFuncList,
+}
+
+solverOptions = {
+    # Common Parameters
+    'gridFile': 'mdo_tutorial_rans.cgns',
+    'restartFile': 'output/mdo_tutorial_vol.cgns',
+    'outputDirectory':'output',
+
+    # Physics Parameters
+    'equationType':'RANS',
+    'useblockettes': False,
+
+    'turbulenceModel': 'Menter SST',
+    "turbResScale": [1e3, 1e-8],
+
+    "nsubiter": 3,
+    "nsubiterturb": 20,
+
+    # ANK
+    "useANKSolver": True,
+    "ANKUseTurbDADI": True,
+    "ANKADPC": False,
+
+    "monitorVariables": ["resrho", "totalr", "cl", "cd"],
+    "L2Convergence": 1e-15,
+    "adjointL2Convergence": 1e-16,
+
+    "solutionPrecision": "double",
+    "outputSurfaceFamily": "wall",
+}
+
+au = ADFLOW_UTIL(aeroOptions, solverOptions, options)
+au.run()
